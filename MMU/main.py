39277138c9924ffe7b71ae0812b6b33709317a9d
AasthaRanjan <aastharanjan0212@gmail.com>
2017-08-09 21:55:45 +0530

    5th day work done

--- MMU/main.py
+++ MMU/main.py
@@ -1,40 +1,48 @@
+from spy_details import spy_name , spy_salutation , spy_age , spy_ratings
+import start_chat
 print "Let's get started!"
-spy_name = raw_input("provide your name here SPY :")
+question = "continue as " + spy_salutation + " " + spy_name + ": (Y/N)"
+existing = raw_input(question)
+# validating user input
+if existing == "Y" or existing == "y":
+   start_chat.start_chat(spy_name, spy_age, spy_ratings)
+elif(existing == "N" or existing == "n"):
+    spy_name = raw_input("provide your name here SPY :")
+    if len(spy_name)>0:
+        if not spy_name.isalpha():
+            print " invalid spy_name INPUT "
+            spy_name = raw_input("provide your name here again :")
+        else:
+            print "VALID INPUT NAME "
+            spy_age = 0
+            spy_rating = 0.0
+            spy_is_online = False
+            spy_salutation = raw_input("what should we call you? (Mr. or Ms.):")
+            spy_age = int(raw_input("Enter your age here:"))
+            if spy_age >12 and spy_age <50:
+                print "you are too good to go."
+                spy_rating = float(raw_input("Give your rating here:"))
+                if spy_rating >= 4.5:
+                    print "good"
+                elif spy_rating > 3.5 and  spy_rating > 4.5:
+                    print "excellent"
+                elif spy_rating > 2.5 and spy_rating > 3.5:
+                    print "you can do better"
+                else:
+                    print "need improvment"
+                spy_is_online = True
+                print "Authentication completed welcome " + spy_name + " age: " + str(spy_age) + " rating of: " + str(spy_rating) + " proud to have you!!"
+            else:
+                print "INVALID USER"
 
-#check whether spy has input something or not
-#concatination of salutation and name.
-if len(spy_name)>0:
-
-    spy_age = 0
-    spy_rating = 0.0
-    spy_is_online = False
-    spy_salutation = raw_input("what should we call you? (Mr. or Ms.):")
-    spy_age = int(raw_input("Enter your age here:"))
-    if spy_age >12 and spy_age <50:
-        print "you are too good to go."
     else:
         print "You are not a valid user"
 
-        spy_rating = raw_input("Give your rating here:")
-        if spy_rating > 4.5 :
-
-elif spy_rating > 2.5 and spy_rating <3.5:
-
-    print type(spy_age)
-    spy_name = spy_salutation + " "  + spy_name
+    spy_name = spy_salutation + " " + spy_name
     print  " WLECOME " + spy_name + " GLad to have you back with us!! "
     print " Alright " + spy_name + " I'd like know more about you "
 
-
-
-
-
-else:
     print "A SPY needs to have valid name.Try again."
-
-#
-#name= "Ms. Aastha"
-#if len(name) > 0:
-   # new_message = "YAY!! Name is not empty"
-    #print new_message
+else:
+    print "WRONG CHOICE"
 
